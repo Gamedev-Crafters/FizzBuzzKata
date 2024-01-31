cc4690db8221af01cea01091d0b3a6b743fc2714
Javierdds <deduenassantanojavier@gmail.com>
2024-01-31 22:35:25 +0100

    test: test data class to test fizz buzz game

--- FizzBuzzKata.Tests/FizzBuzzTests.cs
+++ FizzBuzzKata.Tests/FizzBuzzTests.cs
@@ -1,3 +1,5 @@
+using System.Collections;
+
 namespace FizzBuzzKata;
 
 /*
@@ -7,14 +9,40 @@ namespace FizzBuzzKata;
  */
 public class FizzBuzzTests
 {
-    [Fact]
-    public void Fizz_Buzz_Game()
+    [Theory]
+    [ClassData(typeof(TestData))]
+    public void Fizz_Buzz_Game(int valueToPlay, string expected)
     {
         var sut = new FizzBuzz();
-        var playValue = 5;
 
-        var result = sut.PlayFizzBuzz(playValue);
+        var result = sut.PlayFizzBuzz(valueToPlay);
         
-        Assert.Equal("Buzz", result);
+        Assert.Equal(expected, result);
+    }
+
+    private class TestData : IEnumerable<object[]>
+    {
+        private readonly List<object[]> _data = new List<object[]>
+        {
+            new object[] { 1, "1" },
+            new object[] { 2, "2" },
+            new object[] { 3, "Fizz" },
+            new object[] { 4, "4" },
+            new object[] { 5, "Buzz" },
+            new object[] { 6, "Fizz" },
+            new object[] { 7, "7" },
+            new object[] { 8, "8" },
+            new object[] { 9, "Fizz" },
+            new object[] { 10, "Buzz" },
+            new object[] { 11, "11" },
+            new object[] { 12, "Fizz" },
+            new object[] { 13, "13" },
+            new object[] { 14, "14" },
+            new object[] { 15, "FizzBuzz" }
+        };
+
+        public IEnumerator<object[]> GetEnumerator() => _data.GetEnumerator();
+
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
     }
 }
