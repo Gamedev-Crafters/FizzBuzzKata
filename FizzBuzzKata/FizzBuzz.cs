c0c932bd5f8792ae8bd8eeac20fc42a751df35d8
Javierdds <deduenassantanojavier@gmail.com>
2024-01-31 22:37:46 +0100

    feat: fizz buzz game implementation

--- FizzBuzzKata/FizzBuzz.cs
+++ FizzBuzzKata/FizzBuzz.cs
@@ -4,6 +4,18 @@ public class FizzBuzz
 {
     public string PlayFizzBuzz(int playValue)
     {
-        return "Buzz";
+        if (playValue % 5 == 0 && playValue % 3 == 0)
+            return "FizzBuzz";
+        if(playValue % 3 == 0)
+        {
+            return "Fizz";
+        }
+
+        if(playValue % 5 == 0)
+        {
+            return "Buzz";
+        }
+
+        return playValue.ToString();
     }
 }
